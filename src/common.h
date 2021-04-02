ac162b773d568f644227ef069ade7e63ae491fa1
ehgur062300 <ehgur062300@gmail.com>
2021-04-02 11:52:51 +0900

    Revert "Revert "텍스쳐 만들기""
    This reverts commit a7bc28feddc3458b0ee55bd392cf9c26c9d01274.

---
+++ src/common.h
@@ -0,0 +1,19 @@
+#ifndef __COMMON_H__ //만약 define 이 되지 않았다면 4줄부터 갔다붙힌다
+#define __COMMON_H__ //몇번을 include 해도 이미 있기때문에 건너뛴다
+
+#include <memory>
+#include <string>
+#include <optional>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <spdlog/spdlog.h>
+
+#define CLASS_PTR(klassName) \
+class klassName; \
+using klassName ## UPtr = std::unique_ptr<klassName>; \
+using klassName ## Ptr = std::shared_ptr<klassName>; \
+using klassName ## WPtr = std::weak_ptr<klassName>;
+
+std::optional<std::string> LoadTextFile(const std::string& filename);
+
+#endif // __COMMON_H__
